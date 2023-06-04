4e24fdf95153aff9ccb72647cea573fa2de386e5
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-06-04 18:36:50 -0400

    added greet method

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,16 +1,30 @@
-const person = {
-    name: 'Joseph',
+const movieList = document.getElementById('movie-list');
+
+movieList.style['background-color'] = 'red';
+movieList.style.display = 'block'
+
+const userChosenKeyName = 'level'
+
+let person = {
+    'first name': 'Joseph',
     age: 52,
     hobbies: ['Sports', 'Cooking'],
+    [userChosenKeyName]:'One',
     greet: function(){
         alert('Hello you')
-    }
+    },
+    1.5: 'hello'
+
 }
 
 // person.age = 53;
-// delete person.age;
+delete person.age;
 // person.age = undefined;
 // person.age = null;
 person.isAdmin = true;
 
-console.log(person)
+const keyName = 'first name'
+
+console.log(person[keyName]);
+console.log(person[1.5])
+console.log(person['level'])
