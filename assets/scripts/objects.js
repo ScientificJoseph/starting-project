dcb2d91ef5c1023aefac4edbb0a617771622b70c
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-06-08 19:23:31 -0400

    added filter method to filter movie titles

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -3,7 +3,7 @@ const searchBtn = document.getElementById('search-btn');
 
 const movies = [];
 
-const renderMovies = () => {
+const renderMovies = (filter = '') => {
     const movieList = document.getElementById('movie-list');
     if (movies.length === 0) {
         movieList.classList.remove('visivle')
@@ -12,7 +12,12 @@ const renderMovies = () => {
         movieList.classList.add('visible')
     }
     movieList.innerHTML = '';
-    movies.forEach((movie) => {
+
+    const filteredMovies = !filter 
+    ? movies 
+    : movies.filter(movie => movie.info.title.includes(filter))
+
+    filteredMovies.forEach((movie) => {
         const movieEl = document.createElement('li')
         // movieEl.textContent = movie.info.title;
         let text = movie.info.title + ' - ';
@@ -55,4 +60,11 @@ const addMovieHandler = () => {
     renderMovies()
 };
 
-addMovieBtn.addEventListener('click', addMovieHandler)
+const searchMovieHandler = () => {
+    const filterTerm = document.getElementById('filter-title').value
+    renderMovies(filterTerm)
+}
+
+addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler);
+
